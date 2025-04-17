8f6e18aac5bddb8bcd180ad83ab892612f1c8f9c
Alexandre <alexandredesfosses@hotmail.com>
2025-04-17 16:05:11 -0400

    fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
-        # Add your dependencies here, e.g. "requests", "pyyaml", etc.
+        "colorama~=0.4.6",
     ],
     entry_points={
         "console_scripts": [
